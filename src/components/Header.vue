456ae796981a38e09a4d8f72887830a8e8090d56
Mateo <mateuszmezyk90@gmail.com>
2020-10-18 20:45:07 +0200

    Basic quiz app in Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-nav tabs>
+        <b-nav-item disabled active><b>Fancy quiz app</b></b-nav-item>
+        <b-nav-item>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
